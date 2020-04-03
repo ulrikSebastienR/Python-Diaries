0810fd7874503a4d5a990b823f366150844b24b6
Ulrik Sebastien R <60251089+ulrikSebastienR@users.noreply.github.com>
2020-04-03 14:22:20 +0530

    daily commit

---
+++ pandas on idle.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+
+excel1 = 'worksheet1.xlsx'
+print(excel1)
+
+excel2 = 'worksheet2.xlsx'
+print(excel2)
+
+excel3 = 'worksheetodf.xlsx'
+print(excel3)
+
+df1 = pd.read_excel(excel1)
+df2 = pd.read_excel(excel2)
+
+merge = pd.merge(df1,df2,on ="Date")
+print(merge)
+
+merge.to_excel("output.xlsx")
+
+
+
+
---
+++ pandas on thonny.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+excel1 = 'worksheet1.xlsx'
+print(excel1)
+
+df1 = pd.read_excel(excel1)
+print(df1)
+
