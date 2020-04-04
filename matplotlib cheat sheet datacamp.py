1e9886657c91502ef2c2b21142e84635937150ac
Ulrik Sebastien R <60251089+ulrikSebastienR@users.noreply.github.com>
2020-04-04 13:08:38 +0530

    daily update

--- matplotlib cheat sheet datacamp.py
+++ matplotlib cheat sheet datacamp.py
@@ -1,1 +1,8 @@
 '''trying to emulate cheat sheet of datacamp'''
+
+import matplotlib.pyplot as plt
+x = [1,2,3,4]
+y = [10,20,25,30]
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(x, y, color = 'red', linewidth = 3)
